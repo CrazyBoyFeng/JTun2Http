71ae7f60d82c07f84e17e9a7693e0b7cafa04b79
CrazyBoyFeng <CrazyBoyFeng@Live.Com>
2021-03-12 20:25:05 +0800

    Format

--- src/main/kotlin/com/github/crazyboyfeng/jtun2http/TunGateway.kt
+++ src/main/kotlin/com/github/crazyboyfeng/jtun2http/TunGateway.kt
@@ -16,40 +16,52 @@
 package com.github.crazyboyfeng.jtun2http
 
 import kotlinx.coroutines.*
-import java.io.*
-import java.lang.Runnable
+import java.io.FileDescriptor
+import java.io.FileInputStream
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
 import java.net.SocketAddress
 import java.util.logging.Logger
 
-abstract class TunGateway @Throws(TunInterfaceInvalidException::class) constructor(private val tunInterface: FileDescriptor, private val remoteProxy: SocketAddress): Runnable{
-    class TunInterfaceInvalidException: FileNotFoundException()
+class TunGateway @Throws(TunInterfaceInvalidException::class) constructor(
+    tunInterface: FileDescriptor,
+    remoteProxy: SocketAddress
+) : Runnable {
+    class TunInterfaceInvalidException : FileNotFoundException()
+
     private val log = Logger.getGlobal()
+
     init {
-        if(!tunInterface.valid()){
+        if (!tunInterface.valid()) {
             log.severe("Tunnel interface is invalid!")
             throw TunInterfaceInvalidException()
         }
     }
+
     private val httpProxyClient = HttpProxyClient(remoteProxy)
     private val fromLan = FileInputStream(tunInterface)
     private val toLan = FileOutputStream(tunInterface)
-    private var mainJob: Job?=null
-    @Throws(SecurityException::class) /*
-    kotlin不显式声明异常这点并不好，只有实际使用时遇到了才发现还有权限不足异常。
-    要是idea智能点也好，提示一下可能有的异常，但是什么提示都没有。捕捉异常全靠猜和坑。
-    */
-    override fun run() = runBlocking{//使用线程内调用阻塞协程是保活需要。非阻塞协程不保活。
+    private var mainJob: Job? = null
+
+    @Throws(SecurityException::class)
+    /*
+       kotlin不显式声明异常这点并不好，只有实际使用时遇到了才发现还有权限不足异常。
+       要是idea智能点也好，提示一下可能有的异常，但是什么提示都没有。捕捉异常全靠猜和坑。
+       */
+    override fun run() = runBlocking {//使用线程内调用阻塞协程是保活需要。非阻塞协程不保活。
         //todo 测试http代理连接
         //todo 连接失败抛出异常
         mainJob = launch { listen() }
     }
+
     fun stop() {
         mainJob?.cancel()
     }
-    private suspend fun listen()= withContext(Dispatchers.Unconfined){//当前线程运行协程
+
+    private suspend fun listen() = withContext(Dispatchers.Unconfined) {//当前线程运行协程
         //其实kotlin协程底层就是线程池。比如此处可以翻译为：在当前线程中执行本方法。
         try {
-            while(isActive){//没有被打断
+            while (isActive) {//没有被打断
                 receive()
                 delay(100)
             }
@@ -57,12 +69,14 @@ abstract class TunGateway @Throws(TunInterfaceInvalidException::class) construct
             close()
         }
     }
-    private suspend fun receive()= withContext(Dispatchers.IO){
+
+    private suspend fun receive() = withContext(Dispatchers.IO) {
         //todo 读取并处理
     }
-    private suspend fun close()= withContext(Dispatchers.IO){
-        withContext(Dispatchers.IO){fromLan.close()}
-        withContext(Dispatchers.IO){toLan.close()}
+
+    private suspend fun close() {
+        withContext(Dispatchers.IO) { fromLan.close() }
+        withContext(Dispatchers.IO) { toLan.close() }
         //TODO 关闭proxy连接？
     }
 }
