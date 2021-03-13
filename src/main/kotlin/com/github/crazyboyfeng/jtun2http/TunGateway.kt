7dba36f9994fc8d5d2337aad101d6fe6910f0094
CrazyBoyFeng <CrazyBoyFeng@Live.Com>
2021-03-13 17:47:43 +0800

    Simplify some methods

--- src/main/kotlin/com/github/crazyboyfeng/jtun2http/TunGateway.kt
+++ src/main/kotlin/com/github/crazyboyfeng/jtun2http/TunGateway.kt
@@ -43,38 +43,34 @@ class TunGateway @Throws(TunInterfaceInvalidException::class) constructor(
     private val toLan = FileOutputStream(tunInterface)
     private var mainJob: Job? = null
 
-    @Throws(SecurityException::class)
-    /*
-       kotlin不显式声明异常这点并不好，只有实际使用时遇到了才发现还有权限不足异常。
-       要是idea智能点也好，提示一下可能有的异常，但是什么提示都没有。捕捉异常全靠猜和坑。
-       */
-    override fun run() = runBlocking {//使用线程内调用阻塞协程是保活需要。非阻塞协程不保活。
+    @Throws(SecurityException::class)//kotlin不显式声明异常这点并不好，只有实际使用时遇到了才发现还有权限不足异常。
+    override fun run() = runBlocking {
+        //使用线程内调用阻塞协程是保活需要。非阻塞协程不保活。
         //todo 测试http代理连接
         //todo 连接失败抛出异常
-        mainJob = launch { listen() }
+        mainJob = launch {
+            //当前线程运行协程
+            try {//要是idea智能点也好，提示一下可能有的异常，但是什么提示都没有。捕捉异常全靠猜和坑。
+                while (isActive) {//没有被打断
+                    receive()
+                    delay(100)
+                }
+            } finally {
+                close()
+            }
+        }
     }
 
     override fun interrupt() {
         mainJob?.cancel()
     }
 
-    private suspend fun listen() = withContext(Dispatchers.Unconfined) {//当前线程运行协程
-        //其实kotlin协程底层就是线程池。比如此处可以翻译为：在当前线程中执行本方法。
-        try {
-            while (isActive) {//没有被打断
-                receive()
-                delay(100)
-            }
-        } finally {//发现此处线程已关闭
-            close()//将在主线程运行
-        }
-    }
-
     private suspend fun receive() = withContext(Dispatchers.IO) {
         //todo 读取并处理
     }
 
-    private suspend fun close() = withContext(NonCancellable) {//排除出主协程的取消操作
+    private suspend fun close() = withContext(NonCancellable) {
+        //排除出主协程的取消操作
         launch(Dispatchers.IO) { fromLan.close() }
         launch(Dispatchers.IO) { toLan.close() }
         //TODO 关闭proxy连接
